589f7e347d7bede3bf1b7ff55513eb549b1e1f61
Shane <sbarbett@ip-10-0-0-239.ec2.internal>
2023-07-28 17:32:27 -0400

    Working on getting the status.io API to work

--- src/status_io/assets/main.js
+++ src/status_io/assets/main.js
@@ -4,26 +4,36 @@
   client.invoke('resize', { width: '100%', height: '200px' });
 
   client.metadata().then(function(metadata) {
+    var headers = {
+      'x-api-key': metadata.settings['API Key'],
+      'x-api-id': metadata.settings['API ID']
+    }
+
     document.getElementById('create-incident').addEventListener('click', function() {
       client.get('ticket').then(function(data) {
         var ticketId = data.ticket.id;
-
-        client.request({
-          url: 'https://api.status.io/1.0/incident/create',
+        var options = {
+          url: 'https://api.status.io/v2/incident/create',
           type: 'POST',
           contentType: 'application/json',
-          headers: {
-            'api_key': metadata['API Key'],
-            'api_id': metadata['API ID']
-          },
+          cors: false,
+          headers: headers,
           data: JSON.stringify({
             statuspage_id: metadata['Status Page ID'],
             incident_name: `Incident for ticket #${ticketId}`,
             incident_details: `Incident created from ZenDesk ticket #${ticketId}`,
             incident_status: 'Investigating'
           })
-        }).then(function(response) {
+        }
+
+        console.log('Headers:', headers)
+        console.log('Options:', options)
+
+        client.request(options).then(function(response) {
+          console.log('Response:', response)
           client.set(`ticket.customField:custom_field_${response.incident.id}`, response.incident.id);
+        }).catch(function(error) {
+          console.log('Error:', error)
         });
       });
     });
@@ -33,13 +43,11 @@
         var incidentId = data.ticket[`customField:custom_field_${incident.id}`];
 
         client.request({
-          url: `https://api.status.io/1.0/incident/resolve/${incidentId}`,
+          url: `https://api.status.io/v2/incident/resolve/${incidentId}`,
           type: 'POST',
           contentType: 'application/json',
-          headers: {
-            'api_key': metadata['API Key'],
-            'api_id': metadata['API ID']
-          },
+          cors: false,
+          headers: headers,
           data: JSON.stringify({
             statuspage_id: metadata['Status Page ID'],
             incident_id: incidentId,
